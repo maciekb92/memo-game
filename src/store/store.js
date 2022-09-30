76906b464595811df25b0baa3bebc04973763a98
Maciej Bojek <bojek.maciej@gmail.com>
2022-09-30 20:46:47 +0200

    Add persistor

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,25 @@ import { compose, applyMiddleware } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './root-reducer';
 import logger from 'redux-logger';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
 
-const middleWares = [
-    process.env.NODE_ENV === 'development' && logger
-].filter(Boolean);
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+  Boolean
+);
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = configureStore({reducer: rootReducer}, undefined, composedEnhancers);
+export const store = configureStore({
+  reducer: persistedReducer,
+  composedEnhancers
+});
+
+export const persistor = persistStore(store);
